8c60ca5f66a77a903893fe949e30e59250dd476f
nich_otieno <nich.otieno@gmail.com>
2025-01-15 21:42:23 +0300

    Adds paginated data table for brand display in All Brands screen

--- lib/features/brands/presentation/all_brands/widgets/brand_table.dart
+++ lib/features/brands/presentation/all_brands/widgets/brand_table.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_flutter_web/utils/paginated_data_table.dart';
 import 'package:flutter/material.dart';
 
 class BrandTable extends StatelessWidget {
@@ -5,6 +6,15 @@ class BrandTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AppPaginatedDataTable(
+     
+      columns: const [
+        DataColumn(label: Text('ID')),
+        DataColumn(label: Text('Name')),
+        DataColumn(label: Text('Description')),
+        DataColumn(label: Text('Actions')),
+      ],
+      source: BrandDataSource(),
+    );
   }
 }
